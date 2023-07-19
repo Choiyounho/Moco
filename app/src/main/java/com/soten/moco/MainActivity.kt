b40ae9e53e875c607bbbe0c7fd94bb82f852ff48
ChoiYounho <maxosa78@gmail.com>
2023-07-19 18:08:33 +0900

    feat: 메인택이티비 수집 구현

--- app/src/main/java/com/soten/moco/MainActivity.kt
+++ app/src/main/java/com/soten/moco/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.soten.moco
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.coin.collectLatest {
+                    Toast.makeText(baseContext, "방출", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
