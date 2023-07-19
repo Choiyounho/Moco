3ff61d7f99c72ea64458f254df8f814e0d26e984
ChoiYounho <maxosa78@gmail.com>
2023-07-19 17:07:40 +0900

    feat: RetrofitFactory 구현

---
+++ data/src/main/java/com/soten/data/network/retrofit/RetrofitFactory.kt
@@ -0,0 +1,27 @@
+package com.soten.data.network.retrofit
+
+import com.soten.data.network.retrofit.Url.UPBIT_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+internal object RetrofitFactory {
+
+    fun create(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(UPBIT_URL)
+            .client(createOkhttpClient())
+            .addCallAdapterFactory(ApiResultCallAdapterFactory())
+            .addConverterFactory(KotlinxConverterFactory.create())
+            .build()
+    }
+
+    private fun createOkhttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY)
+            )
+            .build()
+    }
+}
