e2b7b5801511b9633ae2a122f6a2cbd94c01535a
ChoiYounho <maxosa78@gmail.com>
2023-07-19 17:09:55 +0900

    feat: NetworkModule 의존성 주입

--- data/src/main/java/com/soten/data/di/NetworkModule.kt
+++ data/src/main/java/com/soten/data/di/NetworkModule.kt
@@ -1,4 +1,24 @@
 package com.soten.data.di
 
-object NetworkModul {
+import com.soten.data.network.api.UpbitApi
+import com.soten.data.network.retrofit.RetrofitFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object NetworkModule {
+
+    @Provides
+    fun providesRetrofit(): Retrofit {
+        return RetrofitFactory.create()
+    }
+
+    @Provides
+    fun providesUpbitApi(retrofit: Retrofit): UpbitApi {
+        return retrofit.create(UpbitApi::class.java)
+    }
 }
