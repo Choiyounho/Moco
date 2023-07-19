57d36cf084599fefd1357e439fcaf1e3dde6051c
ChoiYounho <maxosa78@gmail.com>
2023-07-19 18:06:19 +0900

    feat: CoinRepositoryImpl 구현

---
+++ data/src/main/java/com/soten/data/repository/CoinRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.soten.data.repository
+
+import com.soten.data.mapper.toDomain
+import com.soten.data.network.ApiResult
+import com.soten.data.network.api.UpbitApi
+import com.soten.domain.model.Coin
+import com.soten.domain.repository.CoinRepository
+import javax.inject.Inject
+
+internal class CoinRepositoryImpl @Inject constructor(
+    private val upbitApi: UpbitApi
+) : CoinRepository {
+
+    override suspend fun fetchAllCoin(): Result<List<Coin>> {
+        val apiResult = upbitApi.fetchAllCoins()
+
+        return when (apiResult) {
+            is ApiResult.Success -> {
+                val value = apiResult.value?.map {
+                    it.toDomain()
+                } ?: emptyList()
+                Result.success(value)
+            }
+
+            is ApiResult.Failure -> Result.failure(IllegalStateException("code = ${apiResult.code}, error = ${apiResult.message}"))
+            is ApiResult.NetworkError -> Result.failure(apiResult.exception)
+            is ApiResult.Unexpected -> Result.failure(apiResult.t ?: UnknownError())
+        }
+    }
+}
