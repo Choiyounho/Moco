ec44538e65ac359b7c7143d524f3a0ad291c4c13
ChoiYounho <maxosa78@gmail.com>
2023-07-20 17:28:37 +0900

    feat: BaseActivity 설계

---
+++ app/src/main/java/com/soten/moco/base/BaseActivity.kt
@@ -0,0 +1,32 @@
+package com.soten.moco.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+open class BaseActivity<DB : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int,
+) : AppCompatActivity() {
+
+    protected lateinit var binding: DB
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setBinding()
+
+        initViews()
+        bindViews()
+    }
+
+    open fun initViews() = Unit
+    open fun bindViews() = Unit
+
+    private fun setBinding() {
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+
+        binding.lifecycleOwner = this
+    }
+}
