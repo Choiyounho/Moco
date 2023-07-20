49641315211b937313b287199c1fe38a49e24253
ChoiYounho <maxosa78@gmail.com>
2023-07-20 17:30:28 +0900

    feat: MainActivity, Base 적용

--- app/src/main/java/com/soten/moco/MainActivity.kt
+++ app/src/main/java/com/soten/moco/MainActivity.kt
@@ -1,24 +1,23 @@
 package com.soten.moco
 
-import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.soten.moco.base.BaseActivity
+import com.soten.moco.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     private val mainViewModel by viewModels<MainViewModel>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun observeData() {
+        super.observeData()
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
